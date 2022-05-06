fc79fbb78e3677de15df751e453a8bf8b73aa707
hanescargot <hanescargot@gmail.com>
2022-05-06 17:55:30 +0900

    init

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) {
+            System.out.println( "JEROEN"); //"56"  /"JAN" 23
+           
+	}
+}
+
+
+
+class Solution {
+
+}
